fc023172c164a786eaa61139a8603aea040167fa
amashadahamni <amashadahamni16@gmail.com>
2024-05-27 11:32:22 +0530

    ggg

---
+++ src/Book.java
@@ -0,0 +1,17 @@
+public class Book {
+
+    public static void main(String[] args) {
+
+        String BookName = "Harry Potter";
+        String BookID = "10205663";
+        String Author = "J.K.Rowling";
+
+
+
+
+        System.out.println("Book Name\t\t:" + BookName);
+        System.out.println("Book ID\t\t:" + BookID);
+        System.out.println("Author\t\t:" + Author);
+
+    }
+}
---
+++ src/LibrarySystem.java
@@ -0,0 +1,16 @@
+public class LibrarySystem {
+
+    public static void main(String[] args) {
+
+        String LibName = "Anna Jones";
+        String LibID = "1025";
+
+
+
+
+
+        System.out.println("Librarian Name\t\t:" + LibName);
+        System.out.println("Librarian ID\t\t:" + LibID);
+
+    }
+}
